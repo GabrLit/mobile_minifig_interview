b598b368debca67d1015a9beaa5111f21c4490a2
GabrLit <litwinski.gabriel@gmail.com>
2023-02-21 13:12:12 +0100

    Fix: fixed tests, moved mockups to inside the test so they are not run as test

--- src/screens/Details/__tests__/DetailsView.test.tsx
+++ src/screens/Details/__tests__/DetailsView.test.tsx
@@ -2,7 +2,15 @@ import { render } from "@testing-library/react-native";
 import { Provider } from "react-redux";
 import { store } from "../../../store/store";
 import DetailsView from "../DetailsView";
-import { mockedMinifig } from "./mockups";
+
+export const mockedMinifig = {
+  set_num: "fig-000029",
+  name: "Harry Potter, Open Dark Blue Jacket over White Shirt, Dark Bluish Gray Legs",
+
+  set_img_url: "https://cdn.rebrickable.com/media/sets/fig-000029/60572.jpg",
+  set_url:
+    "https://rebrickable.com/minifigs/fig-000029/harry-potter-open-dark-blue-jacket-over-white-shirt-dark-bluish-gray-legs/",
+};
 
 const mockProps = {
   selectedMinifig: mockedMinifig,
